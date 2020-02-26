eeee6cbef480da773975e6ff3bf6d9a1fee2ca2e
Nikola <nikolaekmedzic@yahoo.com>
2020-02-26 21:26:51 +0100

    Implemented the weather route

--- app.js
+++ app.js
@@ -12,12 +12,47 @@ var port = 3000 || process.env.PORT ;
 app.use(express.static(path.join(__dirname, 'src')));
 app.use(express.static(__dirname));
 
+// API 
+const geoCode = require("./src/utils/geoCode.js");
+const forecast = require("./src/utils/forecast.js");
+
 
 //ROUTES
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/src/index.html"));
 });
+
+
+app.get("/weather",(req,res)=>{
+
+  if(req.query.address){
+
+    geoCode(req.query.address, (error, geoData)=>{
+        if(error){return res.send({error:"Error in Geocode"})}
+     
+        forecast(geoData.latitude, geoData.longitude, (error, forecastData) => {
+         if(error){return res.send({error:"Error in DarkSky api"});}
+         res.send({
+            forecast:forecastData,
+           
+            location:req.query.address
+         });
+       });
+   
+   });
+ 
+  }else{
+
+  res.send({
+     error:"You must provide the location"
+  });
+
+  }
+
+});
+
+
 
 app.get("/about",(req,res)=>{
   res.sendFile(path.join(__dirname,"/src/About.html"));
