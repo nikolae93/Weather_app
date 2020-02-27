d1343c6419ae35ab86bb6f26547ac89ec0bca5f3
Nikola <nikolaekmedzic@yahoo.com>
2020-02-27 11:43:07 +0100

    Add weather page

--- src/weather.js
+++ src/weather.js
@@ -18,3 +18,26 @@ function transformToAssocArray( prmstr ) {
 var params = getSearchParameters();
 
 console.log(params);
+
+// Dom elements
+
+let loca = document.getElementById("location");
+loca.innerHTML = `Location : ${params.location}`;
+
+let timezone = document.getElementById("timezone");
+timezone.innerHTML = `Timezone : ${params.timezone}`;
+
+let latii = document.getElementById("latitude");
+latii.innerHTML = `Latitude : ${params.lat}`;
+
+let longii = document.getElementById("longitude");
+longii.innerHTML = `Longitude : ${params.lon}`;
+
+let summary = document.getElementById("summary");
+summary.innerHTML = `Summary : ${params.sum}`;
+
+let temperature = document.getElementById("temperature");
+temperature.innerHTML = `Temperature: ${params.t} degrees`;
+
+let rain = document.getElementById("rain");
+rain.innerHTML = `Rain probability: ${params.r}`;
