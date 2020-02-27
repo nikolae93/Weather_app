55f1487250212d628e5bed7ae9334b7ca73d245d
Nikola <nikolaekmedzic@yahoo.com>
2020-02-27 12:03:34 +0100

    Fix image src issue

--- src/weather.js
+++ src/weather.js
@@ -17,7 +17,7 @@ function transformToAssocArray( prmstr ) {
 
 var params = getSearchParameters();
 
-console.log(params);
+//console.log(params);
 
 // Dom elements
 
@@ -41,3 +41,31 @@ temperature.innerHTML = `Temperature: ${params.t} degrees`;
 
 let rain = document.getElementById("rain");
 rain.innerHTML = `Rain probability: ${params.r}`;
+
+// DOM ends
+
+// JS change icon according to icon param
+
+let icon = params.icon;
+let iconDOM = document.getElementById("wimg");
+
+console.log(icon);
+//console.log(iconDOM);
+
+if(icon === "clear-day"){
+   iconDOM.scr="./img/clear-day.jpg";
+}else if(icon === "clear-night"){
+  iconDOM.scr="./img/clear-night.jpg";
+}else if(icon === "rain"){
+  iconDOM.src="./img/rain.png";
+}else if(icon === "snow"){
+  iconDOM.src="./img/snow.png";
+}else if(icon === "sleet"){
+  iconDOM.src="./img/sleet.png";
+}else if(icon === "fog"){
+  iconDOM.src="./img/fog.png";
+}else if(icon === "cloudy"){
+  iconDOM.src="./img/cloudy.png";
+}else{
+  iconDOM.src="./img/partly-cloudy.png";
+}
